08e2987bb0f1f51f2310b4a8044099d105ae744a
Suhani Gupta <144280247+suhanigupta23@users.noreply.github.com>
2025-02-21 23:06:02 +0530

    Block types : instance and static blocks

--- Block_types.java
+++ Block_types.java
@@ -1,9 +1,6 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
-class Main {
+class Block_types {
     
-    Main(){
+   Block_types(){
         System.out.println("This is default constructor.");
     }
     
@@ -13,10 +10,16 @@ class Main {
     static{     //OBJECT CREATION NOT NEEDED TO EXECUTE STATIC BLOCK
         System.out.println("Static block content this side!!");
     }
+    static void show{
+        System.out.println("Hello there this is static but printed at last.As called at last .");
+    }
 //STATIC SABSE PEHLE CHALEGA , THEN INSTANCE , THEN DEFAULT CONSTRUCTOR.
     public static void main(String[] args) {
         System.out.println("Try programiz.pro");
         //instance block called.
-        Main r= new Main();
+        Block_types r= new Block_types();
+        
+        //IF STATIC TO CALL LATER THEN : 
+        Block_types.show();
     }
 }
