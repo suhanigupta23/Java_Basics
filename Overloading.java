92a847939878b55de8ac2ec91a4bde047467741d
Suhani Gupta <144280247+suhanigupta23@users.noreply.github.com>
2025-02-23 13:55:07 +0530

    Compile time polymorphism : Overloading

---
+++ Overloading.java
@@ -0,0 +1,26 @@
+class Overloading{
+    void add()
+    {
+        int a=10, b=20,c;
+        c=a+b;
+        System.out.println(c);
+    }
+    void add(int x, int y)
+    {
+        int c;
+        c=x+y;
+        System.out.println(c);
+    }
+    void add(int x, double y)
+    {
+        double c;
+        c=x+y;
+        System.out.println(c);
+    }
+    public static void main(String args[]){
+        Overloading r= new Overloading();
+        r.add();
+        r.add(100,200);
+        r.add(45,67.7);
+    }
+}
